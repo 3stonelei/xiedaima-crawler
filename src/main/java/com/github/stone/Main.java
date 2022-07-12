f2978b010879d70712662ef5361fc5821f3655cd
3stonelei <1462070965@qq.com>
2022-07-12 12:09:12 +0800

    爬虫项目基本配置

---
+++ src/main/java/com/github/stone/Main.java
@@ -0,0 +1,29 @@
+package com.github.stone;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+/**
+ * @author Lilei
+ * @date 2022/7/12-@11:13
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://sina.cn");
+        try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            // do something useful with the response body
+            // and ensure it is fully consumed
+            System.out.println(EntityUtils.toString(entity1));
+        }
+
+    }
+}
---
+++ src/test/java/com/github/stone/SmokeTest.java
@@ -0,0 +1,14 @@
+package com.github.stone;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Lilei
+ * @date 2022/7/12-@11:26
+ */
+public class SmokeTest {
+    @Test
+    public void test(){
+
+    }
+}
